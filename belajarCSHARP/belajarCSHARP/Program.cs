19831c15e4008fa5b66c9526d9344e52a79c3e1d
Muhamad Reza Pahlepi <anonymousconnection1995@gmail.com>
2019-03-22 14:53:40 +0700

    Add files via upload

---
+++ belajarCSHARP/belajarCSHARP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace belajarCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukan Nama karyawan :");
+            string karyawan = Console.ReadLine();
+        
+            Console.WriteLine("isi jumlah lembur berdasarkan hari (1-10) :");
+
+            int lembur = int.Parse(Console.ReadLine());
+
+            int bonus = 25000 * lembur;
+            int gaji = 4000000 + bonus;
+
+            Console.WriteLine();
+            Console.WriteLine("Nama Karyawan : " + karyawan);
+            Console.WriteLine("Jumlah lembur : " + lembur);
+            Console.WriteLine("Jumlah Gaji + Lembur : " + gaji);
+
+            Console.WriteLine();
+
+            Console.Write("Press Any key to Continue!");
+            Console.ReadKey(true);
+        }
+    }
+}
